08845af71ea7c71943ed3a11bdb9644292a01c49
HashimGharip <hgharip21@gmail.com>
2026-03-25 14:51:05 +0200

    add get_video_ids function to get all videos from playlist

--- video_stats.py
+++ video_stats.py
@@ -8,7 +8,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY=os.getenv("API_KEY")
 CHANNEL_HANDEL="MrBeast"
-
+maxReuslts=50
 
 def get_playlistid():
     try:
@@ -28,6 +28,34 @@ def get_playlistid():
         return channel_playlistid
     except requests.exceptions.RequestException as e:
         raise e
+playlistId=get_playlistid()
 
+def get_video_ids(playlistid):
+    video_ids=[]
+    pageToken=None
+    base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxReuslts}&playlistId={playlistId}&key={API_KEY}"
+    try:
+        while True:
+            url=base_url
+            if pageToken:
+                url +=f"&pageToken={pageToken}"
+            
+            response=requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+
+            for item in data.get('items',[]):
+                video_id=item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            
+            pageToken=data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
 if __name__=="__main__":
-    get_playlistid()
+     playlistId=get_playlistid()
+     get_video_ids(playlistId)
