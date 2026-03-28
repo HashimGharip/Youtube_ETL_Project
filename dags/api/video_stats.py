36c03f77a1c4594b5c53557101037c73f628362b
HashimGharip <hgharip21@gmail.com>
2026-03-28 20:05:52 +0200

    Update video_stats file

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,19 +1,26 @@
 import requests
 import json
-import os
+
 from datetime import date
 
-from dotenv import load_dotenv 
+#import os
+#from dotenv import load_dotenv 
+#load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY=os.getenv("API_KEY")
-CHANNEL_HANDEL="MrBeast"
+#API_KEY=os.getenv("API_KEY")
+#CHANNEL_HANDLE="MrBeast"
+API_KEY=Variable.get("API_KEY")
+CHANNEL_HANDLE=Variable.get("CHANNEL_HANDLE")
 maxReuslts=50
 
+# A Python decorator is a way to modify or extend the behavior of a function (or class) without changing its actual code. example @task
+@task
 def get_playlistid():
     try:
-        url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDEL}&key={API_KEY}"
+        url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
         response=requests.get(url)
 
@@ -31,7 +38,8 @@ def get_playlistid():
         raise e
 playlistId=get_playlistid()
 
-def get_video_ids(playlistid):
+@task
+def get_video_ids(playlistId):
     video_ids=[]
     pageToken=None
     base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxReuslts}&playlistId={playlistId}&key={API_KEY}"
@@ -58,6 +66,7 @@ def get_video_ids(playlistid):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def extract_video_data(video_ids):
 
     extracted_data=[]
@@ -97,7 +106,7 @@ def extract_video_data(video_ids):
     
     except requests.exceptions.RequestException as e:
         raise e
-    
+@task  
 def save_to_json(extracted_data):
     file_path=f"./data/YT_data_{date.today()}.json"
 
